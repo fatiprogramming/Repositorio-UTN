334c43ba00ee291d56a33ff4dca1f2c3d0eb9a6d
Fátima Blanco <fatib2002@hotmail.com>
2024-01-31 08:06:51 -0300

    Al fin funciona

--- M3U2/app.js
+++ M3U2/app.js
@@ -1,54 +1,51 @@
-const express = require("express");
-const session = require("express-session");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const app = express();
-const port = process.env.PORT || 3000;
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var presentacionRouter = require("./routes/presentacion");
+var proyectosRouter = require("./routes/proyectos");
+var faqRouter = require("./routes/faq");
+var contactoRouter = require("./routes/contacto");
 
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+require("dotenv").config();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(
-  session({
-    secret: "clave123",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+app.use("/", indexRouter);
+app.use("/presentacion", presentacionRouter);
+app.use("/proyectos", proyectosRouter);
+app.use("/faq", faqRouter);
+app.use("/contacto", contactoRouter);
+app.use("/users", usersRouter);
 
-app.get("/", function (req, res) {
-  const conocido = Boolean(req.session.nombre);
-  res.send(`
-    <h1>Sesiones en Express.js</h1>
-    <p>Conocido: ${conocido}</p>
-    <p>Nombre: ${req.session.nombre}</p>
-    `);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-app.post("/ingresar", function (req, res) {
-  if (req.body.nombre) {
-    req.session.nombre = req.body.nombre;
-  }
-  res.redirect("/");
-});
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-app.get("/salir", function (req, res) {
-  req.session.destroy();
-  res.redirect("/");
-});
-
-app.get("/escribir-variable/:valor", function (req, res) {
-  const valor = req.params.valor;
-  req.session.miVariable = valor;
-  res.send("Variable de sesión escrita: " + valor);
-});
-
-app.get("/mostrar-variable", function (req, res) {
-  const valor = req.session.miVariable;
-  res.send("Variable de sesión actual: " + valor);
-});
-
-app.listen(port, () => {
-  console.log("Servidor escuchando en el puerto " + port);
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
