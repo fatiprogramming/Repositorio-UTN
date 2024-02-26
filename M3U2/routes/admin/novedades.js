212b4ba4eca82c510807b5e4ce7584d840154323
Fátima Blanco <fatib2002@hotmail.com>
2024-02-26 10:07:30 -0300

    CRUD pt1

--- M3U2/routes/admin/novedades.js
+++ M3U2/routes/admin/novedades.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', {
     layout: '/admin/layout',
     persona: req.session.nombre,
+    novedades,
   });
 });
-
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadById();
+  res.redirect('/admin/novedades');
+});
 module.exports = router;
--- M3U2/routes/index.js
+++ M3U2/routes/index.js
@@ -1,14 +1,16 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-
-var nodemailer = require("nodemailer");
+var novedadesModel = require('../models/novedadesModel');
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "" });
+router.get('/', async function (req, res, next) {
+  novedades = await novedadesModel.getNovedades();
+  novedades = novedades.splice(0, 5);
+  res.render('index', { novedades });
 });
 
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   var nombre = req.body.nombre;
   var email = req.body.email;
   var transporter = nodemailer.createTransport({
@@ -21,13 +23,13 @@ router.post("/", async (req, res, next) => {
   });
 
   var obj = {
-    to: "mail@mail.com",
-    subject: "Formulario de contacto",
+    to: 'mail@mail.com',
+    subject: 'Formulario de contacto',
     html: `${nombre} desea contactarse mediante este correo: ${email}`,
   };
   var info = await transporter.sendMail(obj);
-  res.render("index", {
-    message: "Solicitud de contacto enviada",
+  res.render('index', {
+    message: 'Solicitud de contacto enviada',
   });
 });
 
