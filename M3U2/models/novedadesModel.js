381565e3ee1d33b7da3e6386b04e8a4b2c61f255
Fátima Blanco <fatib2002@hotmail.com>
2024-03-04 08:39:17 -0300

    Agregar y modificar novedades

--- M3U2/models/novedadesModel.js
+++ M3U2/models/novedadesModel.js
@@ -10,4 +10,37 @@ async function deleteNovedadById(id) {
   var rows = await pool.query(query, [id]);
   return rows;
 }
-module.exports = { getNovedades, deleteNovedadById };
+
+async function insertNovedad(obj) {
+  try {
+    var query = 'insert into novedades set ? ';
+    var rows = await pool.query(query, [obj]);
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+async function getNovedadById(id) {
+  var query = 'SELECT * FROM novedades WHERE id = ?';
+  var rows = await pool.query(query, [id]);
+  return rows[0];
+}
+
+async function modificarNovedadById(obj, id) {
+  try {
+    var query = 'UPDATE novedades SET ? WHERE id = ?';
+    var rows = await pool.query(query, [obj, id]);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getNovedades,
+  deleteNovedadById,
+  insertNovedad,
+  getNovedadById,
+  modificarNovedadById,
+};
--- M3U2/routes/admin/novedades.js
+++ M3U2/routes/admin/novedades.js
@@ -16,4 +16,61 @@ router.get('/eliminar/:id', async (req, res, next) => {
   await novedadesModel.deleteNovedadById();
   res.redirect('/admin/novedades');
 });
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', { layout: 'admin/layout' });
+});
+
+router.post('/agregar', async (req, res, next) => {
+  try {
+    if (
+      req.body.titulo !== '' &&
+      req.body.subtitulo !== '' &&
+      req.body.cuerpo !== ''
+    ) {
+      await novedadesModel.insertNovedad(req.body);
+      return res.redirect('/admin/novedades');
+    } else {
+      return res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargó la novedad',
+    });
+  }
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad,
+  });
+});
+router.post('/modificar', async (req, res, next) => {
+  try {
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo,
+    };
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  } catch (error) {
+    console.log(error);
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modificó la novedad',
+    });
+  }
+});
 module.exports = router;
